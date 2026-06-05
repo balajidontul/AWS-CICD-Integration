383908e2ad84064f8721ad75b021a13e4fc01b47
CICDUser-at-200810848079 <X2NGFjlAhoVpM6uT3jK+bwzuoKoxXJPvuRxpaemNrG9gaFeRNQPaHGEQNuA=>
2026-06-05 21:17:06 +0530

    first msg

---
+++ src/main/java/com/balajidontul/awscicdintegration/AwsCicdIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.balajidontul.awscicdintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsCicdIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsCicdIntegrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/balajidontul/awscicdintegration/AwsCicdIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.balajidontul.awscicdintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsCicdIntegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
